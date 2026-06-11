31ff3250a6c6c17179b45d3e863a58c748e24f7d
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-11 16:34:24 -0300

    Criado os comandos fundamentais do js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,65 @@
-console.log("teste")
+console.log("teste");
+
+/*Coméntario de múltiplas linhas,ou seja posso 
+escrever, posso desrever algo nesse tipo de comentário
+*/
+
+//Criando Variáveis
+let idade = 17;
+var nome = "Pedro";
+const altura = 1.80
+let admin = false
+let moderador = false
+
+console.log("Sua idade é: " + idade);
+console.log('Seu nome é: ',nome);
+console.log(`Sua altura é:   ${altura}`); 
+
+//Teste do If
+if (idade >=18) {
+     console.log("Maior de Idade")  
+}  else {
+    console.log("Menor de Idade")
+}
+
+if (altura > 1.75) {
+    console.log("Acima da Média")
+} else if (altura = 1.75) {
+    console.log("Média")
+} else {
+    console.log("Abaixo da Média")
+}
+
+//Operadores Matemáticos
+/*
+    + SOMA
+    - SUBTRAÇÃo
+    * MULTIPLICAÇÃO
+    / DIVISÃO
+    % MÓDULO(RESTO DA DIVISÃO)
+*/
+
+//Operadores Relacionais
+/*
+    = Atribuição
+    == Comparação ou igual
+    === Comparação estrita (compara tanto o valor quanto o tipo se é string ou number)
+    > Maior que
+    < Menor que
+    >= Maior ou Igual
+    <= Menor ou Igual
+    != Diferente
+*/
+
+//Operadores Lógico
+/*
+     && E
+    || Ou
+*/
+
+//Teste do ||
+if(admin == true || moderador == true) {
+    console.log("Acesso Permitido")
+} else {
+    console.log("Acesso Negado")
+}
