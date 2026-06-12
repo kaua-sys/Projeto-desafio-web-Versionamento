87d4cf394ef39dc1fc468b977caf83a31900dca4
Luiz Kauã <lkaua4546@gmail.com>
2026-06-12 15:52:49 -0300

    Resolvido alguns problemas no script

--- js/script_idade.js
+++ js/script_idade.js
@@ -6,4 +6,20 @@ const divResultado = document.querySelector('#div-dados')
 formDados.addEventListener('submit', (evt)=>{
     evt.preventDefault()
 
+    const objFormDados = new FormData (formDados)
+
+    let nome = objFormDados.get('nome')
+    let idade = objFormDados.get('idade')
+
+    let situacaoIdade = ''
+
+
+    if (idade >= 18){
+        situacaoIdade = `${nome},você é maior de idade`
+    }else{
+        situacaoIdade = `${nome}, você é menor de idade`
+    }
+
+    divResultado.innerHTML = situacaoIdade
+
 })
