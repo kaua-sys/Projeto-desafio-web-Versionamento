a08bc4aa64d8da6b28817fa3cf043a8978db3079
Luiz Kauã <lkaua4546@gmail.com>
2026-06-12 14:58:08 -0300

    Adicionado o DOM

---
+++ js/script_idade.js
@@ -0,0 +1,9 @@
+//Pegando Elementos do DOM
+const formDados = document.querySelector('#formulario-pessoa')
+const divResultado = document.querySelector('#div-dados')
+
+//Capturando o evento submit do formulário
+formDados.addEventListener('submit', (evt)=>{
+    evt.preventDefault()
+
+})
